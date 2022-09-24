a5af257393a2254b2d0606846b36f9a175038e4e
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-09-24 18:35:56 -0400

    1 file modified

--- index.js
+++ index.js
@@ -114,6 +114,19 @@ console.log(secondLowest([1, 3, 2, 4, 5]));
 // According to Wikipedia: In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum). Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
 // Example: The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
 
+function perfectNum(num) {
+  var res = 1;
+  for (i = 2; i < num; i += 2) {
+    if ((num / i) % 2 == 0) {
+      res += i;
+      res += num / i;
+    }
+  }
+  return res / 2 == num ? true : false;
+}
+
+console.log(perfectNum(28));
+
 // 13. Write a JavaScript function to compute the factors of a positive integer.
 
 // 14. Write a JavaScript function to convert an amount to coins.
